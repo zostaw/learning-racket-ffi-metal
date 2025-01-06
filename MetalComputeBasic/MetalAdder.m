b584f09a0c4213e53eeee0c9ea09835f7b5c682d
zostaw <zostaw@users.noreply.github.com>
2025-01-06 21:32:49 +0100

    Saving at this point, because I'm going to remove a lot of things that I spent a lot of time figuring out how to do. But basically I'm going to remove all the stuff and replace it with more clear structures

--- MetalComputeBasic/MetalAdder.m
+++ MetalComputeBasic/MetalAdder.m
@@ -123,7 +123,7 @@ const unsigned int bufferSize = arrayLength * sizeof(float);
 
 }
 
-- (void)prepareDataWithInputs:(float*)dataA dataB:(float*)dataB numElements:(size_t)numElements {
+- (void)prepareDataWithFloatInputs:(float*)dataA dataB:(float*)dataB numElements:(size_t)numElements {
     size_t bufferSize = numElements * sizeof(float);
 
     // Allocate three buffers to hold our initial data and the result.
@@ -138,6 +138,9 @@ const unsigned int bufferSize = arrayLength * sizeof(float);
     void* bufferContentsB = [_mBufferB contents];
     memcpy(bufferContentsB, dataB, bufferSize);
 }
+
+
+
 
 - (void) sendComputeCommand
 {
@@ -172,6 +175,127 @@ const unsigned int bufferSize = arrayLength * sizeof(float);
     }
 
     [self verifyResults];
+}
+
+
+enum metal_data_type {
+    FLOAT,
+    INT32,
+};
+
+struct metal_vector {
+              void* buffer_ptr;
+              int buffer_len;
+              enum metal_data_type data_type;
+};
+
+
+__attribute__((visibility("default")))
+bool computeAdd(void* device, void*library, void* bufferA, void* bufferB, void* bufferResult) {
+
+    NSError* error = nil;
+
+    id<MTLDevice>  _mDevice       = (__bridge id<MTLDevice>)device;
+    id<MTLLibrary> _mLibrary      = (__bridge id<MTLLibrary>)library;
+    id<MTLBuffer>  _mBufferA      = (__bridge id<MTLBuffer>)bufferA;
+    id<MTLBuffer>  _mBufferB      = (__bridge id<MTLBuffer>)bufferB;
+    id<MTLBuffer>  _mBufferResult = (__bridge id<MTLBuffer>)bufferResult;
+    // id<MTLBuffer>  _mBufferResult = [_mDevice newBufferWithLength:bufferSize options:MTLResourceStorageModeShared];
+
+    // Validate casted objects
+    if (!_mDevice || !_mLibrary || !_mBufferA || !_mBufferB || !_mBufferResult) {
+        NSLog(@"Error: One or more Metal objects are invalid.");
+        return false;
+    }
+
+
+
+    
+    id<MTLFunction> addFunction = [_mLibrary newFunctionWithName:@"add_arrays"];
+    if (addFunction == nil)
+    {
+        NSLog(@"Failed to find the adder function.");
+        return false;
+    }
+
+    // Create a compute pipeline state object.
+    id<MTLComputePipelineState> _mAddFunctionPSO = [_mDevice newComputePipelineStateWithFunction: addFunction error:&error];
+    if (_mAddFunctionPSO == nil)
+    {
+        //  If the Metal API validation is enabled, you can find out more information about what
+        //  went wrong.  (Metal API validation is enabled by default when a debug build is run
+        //  from Xcode)
+        NSLog(@"Failed to created pipeline state object, error %@.", error);
+        return false;
+    }
+
+    id<MTLCommandQueue> _mCommandQueue = [_mDevice newCommandQueue];
+    if (_mCommandQueue == nil)
+    {
+        NSLog(@"Failed to find the command queue.");
+        return false;
+    }
+
+    NSLog(@"Sending:");
+    // Create a command buffer to hold commands.
+    id<MTLCommandBuffer> _mCommandBuffer = [_mCommandQueue commandBuffer];
+    assert(_mCommandBuffer != nil);
+
+    // Start a compute pass.
+    id<MTLComputeCommandEncoder> _mComputeEncoder = [_mCommandBuffer computeCommandEncoder];
+    assert(_mComputeEncoder != nil);
+
+    encodeAddCommand(_mComputeEncoder, _mAddFunctionPSO, _mBufferA, _mBufferB, _mBufferResult);
+
+    // End the compute pass.
+    [_mComputeEncoder endEncoding];
+
+    // Execute the command.
+    [_mCommandBuffer commit];
+
+    // Normally, you want to do other work in your app while the GPU is running,
+    // but in this example, the code simply blocks until the calculation is complete.
+    [_mCommandBuffer waitUntilCompleted];
+
+
+    float *result = _mBufferResult.contents;
+
+    int numResultsToPrint = 10;
+    NSLog(@"Results:");
+    for (int i = 0; i<numResultsToPrint; i++) {
+        NSLog(@"%f", result[i]);
+    }
+
+    return true;
+}
+
+
+
+void encodeAddCommand(id<MTLComputeCommandEncoder> computeEncoder,
+                      id<MTLComputePipelineState> pipelineState,
+                      id<MTLBuffer> bufferA,
+                      id<MTLBuffer> bufferB,
+                      id<MTLBuffer> bufferResult) {
+
+    // Encode the pipeline state object and its parameters.
+    [computeEncoder setComputePipelineState:pipelineState];
+    [computeEncoder setBuffer:bufferA offset:0 atIndex:0];
+    [computeEncoder setBuffer:bufferB offset:0 atIndex:1];
+    [computeEncoder setBuffer:bufferResult offset:0 atIndex:2];
+
+    MTLSize gridSize = MTLSizeMake(arrayLength, 1, 1);
+
+    // Calculate a threadgroup size.
+    NSUInteger threadGroupSize = pipelineState.maxTotalThreadsPerThreadgroup;
+    if (threadGroupSize > arrayLength)
+    {
+        threadGroupSize = arrayLength;
+    }
+    MTLSize threadgroupSize = MTLSizeMake(threadGroupSize, 1, 1);
+
+    // Encode the compute command.
+    [computeEncoder dispatchThreads:gridSize
+              threadsPerThreadgroup:threadgroupSize];
 }
 
 - (void)encodeAddCommand:(id<MTLComputeCommandEncoder>)computeEncoder {
@@ -224,6 +348,8 @@ const unsigned int bufferSize = arrayLength * sizeof(float);
     printf("Compute results as expected\n");
 }
 
+
+
 // Expose the creation function for FFI
 __attribute__((visibility(("default"))))
 void* createMetalAdder(const char *metallib_full_path) {
@@ -233,20 +359,42 @@ void* createMetalAdder(const char *metallib_full_path) {
 
     NSString *customLibraryPath = [NSString stringWithUTF8String:metallib_full_path];
 
-        MetalAdder* adder = [[MetalAdder alloc] initWithDevice:device customLibraryPath:customLibraryPath];
+    MetalAdder* adder = [[MetalAdder alloc] initWithDevice:device customLibraryPath:customLibraryPath];
 
     return (__bridge_retained void*)adder;  // Return a raw pointer to the MetalAdder object
 }
+
 
 
 __attribute__((visibility(("default"))))
 void* createMetalDevice(const char *metallib_full_path) {
     printf("%s\n", metallib_full_path);
 
-    id<MTLDevice> device = MTLCreateSystemDefaultDevice();
+    id<MTLDevice> mDevice = MTLCreateSystemDefaultDevice();
 
-    return (__bridge_retained void*)device;
+    return (__bridge_retained void*)mDevice;
 }
+
+
+__attribute__((visibility(("default"))))
+void* createMetalLibrary(void* device, const char *metallib_full_path) {
+
+    id<MTLDevice> mDevice = (__bridge id<MTLDevice>)device;
+
+    // 
+    NSString *customLibraryPath = [NSString stringWithUTF8String:metallib_full_path];
+    NSURL *libraryURL = [NSURL fileURLWithPath:customLibraryPath];  // Convert the file path to an NSURL
+    id<MTLLibrary> mLibrary = [mDevice newLibraryWithURL:libraryURL error:nil];  // Use newLibraryWithURL instead of newLibraryWithFile
+
+    if (mLibrary == nil)
+    {
+        NSLog(@"Failed to find the library.");
+        return nil;
+    }
+
+    return (__bridge_retained void*)mLibrary;
+}
+
 
 
 // Expose the computation function for FFI
@@ -261,11 +409,40 @@ void performComputation(void* adder) {
 
 
 __attribute__((visibility("default")))
-void performComputationWithInputs(void* adder, float* dataA, size_t numElements, float* dataB, size_t _numElementsB) {
+void performComputationWithFloatInputs(void* adder, float* dataA, size_t numElements, float* dataB, size_t _numElementsB) {
     MetalAdder* metalAdder = (__bridge MetalAdder*)adder;
-    [metalAdder prepareDataWithInputs:dataA dataB:dataB numElements:numElements];
+    [metalAdder prepareDataWithFloatInputs:dataA dataB:dataB numElements:numElements];
     [metalAdder sendComputeCommand];
     NSLog(@"Execution finished");
+}
+
+
+
+
+__attribute__((visibility("default")))
+void* makeMetalVector(void* device, size_t data_type, size_t numElements) {
+    size_t bufferSize;
+    switch (data_type) {
+        case 0:
+            bufferSize = numElements * sizeof(float);
+        case 1:
+            bufferSize = numElements * sizeof(int);
+            break;
+        default:
+            printf("Wrong type, choose one from:\n - 0 (for float)\n- 1 (for int32)");
+            return NULL;
+    }
+
+    id<MTLDevice> mDevice = (__bridge id<MTLDevice>)device;
+
+    // Allocate buffer
+    id<MTLBuffer> mBufferA = [mDevice newBufferWithLength:bufferSize options:MTLResourceStorageModeShared];
+
+    if (!mBufferA) {
+        return NULL;
+    }
+    
+    return (__bridge_retained void*)mBufferA;
 }
 
 
@@ -288,6 +465,45 @@ void* metalVector(void* device, float* dataA, size_t numElements) {
     memcpy(bufferContentsA, dataA, bufferSize);
 
     return (__bridge_retained void*)mBufferA;
+}
+
+
+
+__attribute__((visibility("default")))
+void* metalInt32Vector(void* device, int* dataA, size_t numElements) {
+
+    size_t bufferSize = numElements * sizeof(int);
+    id<MTLDevice> mDevice = (__bridge id<MTLDevice>)device;
+
+    // Allocate buffer
+    id<MTLBuffer> mBufferA = [mDevice newBufferWithLength:bufferSize options:MTLResourceStorageModeShared];
+
+    if (!mBufferA) {
+        return NULL;
+    }
+    
+    // Copy data into the Metal buffer
+    void* bufferContentsA = [mBufferA contents];
+    memcpy(bufferContentsA, dataA, bufferSize);
+
+    return (__bridge_retained void*)mBufferA;
+}
+
+
+__attribute__((visibility("default")))
+float* getCVector(void* buffer) {
+
+
+    id<MTLBuffer>  _mBuffer  = (__bridge id<MTLBuffer>)buffer;
+
+    if (!_mBuffer) {
+        NSLog(@"Error: One or more Metal objects are invalid.");
+        return false;
+    }
+
+    float *result = _mBuffer.contents;
+
+    return result;
 }
 
 @end
--- main.rkt
+++ main.rkt
@@ -2,6 +2,8 @@
 
 (require ffi/unsafe
          ffi/unsafe/define
+         ffi/cvector
+         ffi/unsafe/cvector
          racket/flonum)
 
 
@@ -12,26 +14,76 @@
   (string-append (path->string (current-directory))
                  "Build/Products/Debug/default.metallib"))
 
+
+(define _metal_data_type
+  (_enum '(FLOAT = 0
+           INT32 = 1)
+         _uint32
+         #:unknown (lambda (x)
+                     (cond [(eq? x 'FLOAT)  0]
+                           [(eq? x 'INT32) 1]
+                           [else (error 'metal_data_type "unknown enum value")]))))
+
+
+(define-cstruct _metal_vector
+                ([buffer_ptr    _pointer]
+                 [buffer_len    _pointer]
+                 [data_type     _metal_data_type))
+
 (define-metal create-metal-device
   (_fun _string -> _pointer)
   #:c-id createMetalDevice)
+
+
+
+(define-metal create-metal-library
+  (_fun _pointer _string -> _pointer)
+  #:c-id createMetalLibrary)
+
+
 
 (define-metal create-metal-adder
   (_fun _string -> _pointer)
   #:c-id createMetalAdder)
 
+
+
 (define-metal perform-computation
   (_fun _pointer -> _void)
   #:c-id performComputation)
 
-(define-metal perform-computation-with-inputs
+
+
+(define-metal compute-add-orig
   (_fun _pointer
-        [vecA : (_vector i _float)]
-        [_int = (vector-length vecA)]
-        [vecB : (_vector i _float)]
-        [_int = (vector-length vecB)]
+        _pointer
+        _pointer
+        _pointer
+        _pointer
+        -> _bool)
+  #:c-id computeAdd)
+
+(define (compute-add mdevice mlibrary mvector-A mvector-B mvector-result)
+  (let ([result (compute-add-orig mdevice mlibrary mvector-A mvector-B mvector-result)])
+    (if (not result)
+        (error "ComputeAdd returned error.")
+        mvector-result)))
+
+
+(define-metal perform-computation-with-float-inputs
+  (_fun _pointer
+        ;; [vecA : (_vector i _float)]
+        ;; [_int = (vector-length vecA)]
+        [vecA : _cvector]
+        [_int = (cvector-length vecA)]
+        ;; [vecB : (_vector i _float)]
+        ;; [_int = (vector-length vecB)]
+        [vecB : _cvector]
+        [_int = (cvector-length vecB)]
         -> _void)
-  #:c-id performComputationWithInputs)
+  #:c-id performComputationWithFloatInputs)
+
+
 
 (define-metal vector->mvector
   (_fun _pointer
@@ -40,27 +92,112 @@
         -> _pointer)
   #:c-id metalVector)
 
-(define (define-mvector mdevice)
+(define (mvector-definer mdevice)
   (lambda (vector) (vector->mvector mdevice vector)))
+
+
+
+(define-metal int32-vector->mvector
+  (_fun _pointer
+        [vec : (_vector i _int32)]
+        [_int = (vector-length vec)]
+        -> _pointer)
+  #:c-id metalInt32Vector)
+
+(define (int32-mvector-definer mdevice)
+  (lambda (vector) (int32-vector->mvector mdevice vector)))
+
+
+
+
+(define-metal cvector->mvector
+  (_fun _pointer
+        ;_cvector
+        [vec : _cvector]
+        [_int = (cvector-length vec)]
+        -> _pointer)
+  #:c-id metalVector)
+
+
+(define _float_ptr
+  (_cpointer _float))
+
+(define-metal mvector->cvector
+  (_fun _pointer
+        -> _float_ptr)
+  #:c-id getCVector)
+
+
+
+
+(define-metal make-mvector-orig
+  (_fun _pointer ; device
+        _int32 ; type-id
+        _int32 ; size
+        -> _pointer)
+  #:c-id makeMetalVector)
+
+(define (make-mutable-mvector mdevice type size)
+  (let ([type-id (match type
+                ['float 0]
+                ['int32 0]
+                [_ (error (format "Type ~a not supported, choose one of: 'float 'int32\n" type))])])
+    (make-mvector-orig mdevice type-id size)))
 
 
 
 
 ;; OO API
 (define adder (create-metal-adder  metallib-path))
-(define dataA (make-vector (expt 2 24) 1.0))
-(define dataB (make-vector (expt 2 24) 2.0))
+;; (define dataA (make-vector (expt 2 24) 1.0))
+(define dataA (list->cvector 
+                (make-list (expt 2 24) 1.0)
+                _float))
+;; (define dataB (make-vector (expt 2 24) 2.0))
+(define dataB (list->cvector 
+                (make-list (expt 2 24) 2.0) 
+                _float))
 
-(perform-computation-with-inputs adder dataA dataB)
+(perform-computation-with-float-inputs adder dataA dataB)
+
+
 
 
 ;; functional API
 (define mdevice (create-metal-device metallib-path))
-(define mvector (define-mvector mdevice))
+(define mlibrary (create-metal-library mdevice metallib-path))
 
-(define mvector-A (mvector (make-vector (expt 2 24) 1.0)))
-(define mvector-B (mvector (make-vector (expt 2 24) 2.0)))
 
+(define vector-size (expt 2 24))
+
+(define cvector-A (list->cvector 
+                    (make-list vector-size 1.0)
+                    _float))
+(define mvector-A (cvector->mvector mdevice cvector-A))
+
+(define cvector-B (list->cvector 
+                    (make-list vector-size 2.0) 
+                    _float))
+(define mvector-B (cvector->mvector mdevice cvector-B))
+
+(define mvector-result (make-mutable-mvector mdevice 'float vector-size))
+
+
+(compute-add mdevice mlibrary mvector-A mvector-B mvector-result)
+
+(define results (mvector->cvector mvector-result))
+
+
+(let ([n 10])
+  (displayln
+ (format "First ~a thingies:  ~a"
+         n
+         (map (λ (i)
+                (ptr-ref results _float i))
+              (range n))
+         )))
+
+;; (perform-computation-with-inputs adder dataA dataB)
 
 (struct Tensor (weights bias forward)
   #:transparent)
